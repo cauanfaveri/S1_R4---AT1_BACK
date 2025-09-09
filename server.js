48738a906b5c8835f80c2e18cf186c0aca1015d5
cauanfaveri <cauanfaveri05@gmail.com>
2025-09-09 08:08:19 -0300

    First commit

--- server.js
+++ server.js
@@ -3,3 +3,17 @@ const app = express();
 const fs = require('fs/promises');
 const port = 2600;
 
+async function adicao(dado, numeros) {
+    try {
+        let dados;
+    } catch (error) {
+        
+    }
+}
+app.get("/soma/:numUm/:numDois", (req, res) => {
+    const numUm = Number(req.params.numUm);
+    const numDois = Number(req.params.numDois);
+    const resultado = numUm + numDois;
+    res.send(`Resultado da soma: ${resultado}`);
+  });
+  
