8977e0856272234d41161eddcf243f6f6ec4cfc0
cauanfaveri <cauanfaveri05@gmail.com>
2025-09-11 08:07:50 -0300

    Salvando pra prox aula

--- server.js
+++ server.js
@@ -3,3 +3,12 @@ const app = express();
 const fs = require('fs/promises');
 const port = 2600;
 
+async function name(params) {
+    
+}
+app.get('/soma/:numeroUm/:numeroDois', (req, res) => {
+    const numUm = Number(req.params.numUm);
+    const numDois = Number(req.params.numDois);
+    const resultado = numUm + numDois;
+    res.send(`Resultado da soma: ${resultado}`)
+});
