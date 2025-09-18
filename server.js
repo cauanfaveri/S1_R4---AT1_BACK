20052227ce334046b94aeb8cd65389613f5b12b0
cauanfaveri <cauanfaveri05@gmail.com>
2025-09-18 08:35:28 -0300

    Commit da soma

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const app = express();
 const fs = require('fs/promises');
-const port = 2600;
+const porta = 2600;
 
-async function name(params) {
-    
-}
-app.get('/soma/:numeroUm/:numeroDois', (req, res) => {
-    const numUm = Number(req.params.numUm);
-    const numDois = Number(req.params.numDois);
-    const resultado = numUm + numDois;
-    res.send(`Resultado da soma: ${resultado}`)
+app.get('/soma/:nUm/:nDois', (req, res) => {
+    const {nUm, nDois} = req.params;
+    const resultado = Number(nUm) + Number (nDois);
+    res.json({ resultado })
+})
+app.listen(porta, () => {
+    console.log(`Exercício de soma na porta ${porta}`);
 });
+
