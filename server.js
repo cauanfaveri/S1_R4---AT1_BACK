65ce0bbfa930c22d4e2fa487b09d4d7e3bb6d310
cauanfaveri <cauanfaveri05@gmail.com>
2025-09-18 10:48:47 -0300

    Fim do Ex4

--- server.js
+++ server.js
@@ -1,3 +1,16 @@
 const express = require('express');
 const app = express();
-const port = 5000;
+const porta = 5000;
+
+app.get('/ano/:ano', async (req, res) => {
+        const {ano} = Number(req.params.ano)
+        const anoBissexto = (ano % 4 === 0 && ano % 100 !== 0) || (ano % 400 === 0);
+    if (anoBissexto){
+    res.send("O ano ${ano} é bissexto")
+    }else{
+    res.send("O ano não é bissexto")
+    }
+});
+app.listen(porta, () => {
+    console.log(`Exercício 3 na porta ${porta}`)
+});
