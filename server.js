3a1aaf1083cca73fafdcf67c7b5e42db8cc6052d
cauanfaveri <cauanfaveri05@gmail.com>
2025-09-18 09:32:43 -0300

    Inicio

--- server.js
+++ server.js
@@ -3,4 +3,9 @@ const app = express();
 const port = 4000;
 const fs = require('fs/promises');
 
-
+app.get('/calculadora', (req, res) => {
+    const {operacao, nUm, nDois} = req.query
+    if (operacao === 'soma'){
+        resultado = Number(nUm) + Number(nDois)
+        return res.json({resultado});
+    });
