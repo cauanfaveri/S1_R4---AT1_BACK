37494d99b0fa09de97a00fc62822cc117250bbb7
cauanfaveri <cauanfaveri05@gmail.com>
2025-09-18 10:03:31 -0300

    Fim ex3

--- server.js
+++ server.js
@@ -1,11 +1,32 @@
 const express = require('express');
 const app = express();
-const port = 4000;
-const fs = require('fs/promises');
+const porta = 4000;
 
-app.get('/calculadora', (req, res) => {
-    const {operacao, nUm, nDois} = req.query
-    if (operacao === 'soma'){
-        resultado = Number(nUm) + Number(nDois)
-        return res.json({resultado});
+app.get('/operacao/:tipo', (req, res) => {
+    const { tipo } = req.params;
+    const { numUm, numDois } = req.query;
+
+    const n1 = parseFloat(numUm);
+    const n2 = parseFloat(numDois);
+    let resultado;
+
+    switch (tipo) {
+        case 'soma':
+            resultado = n1 + n2;
+            break;
+        case 'subtracao':
+            resultado = n1 - n2;
+            break;
+        case 'multiplicacao':
+            resultado = n1 * n2;
+            break;
+        case 'divisao':
+            resultado = n1 / n2;
+            break;
+    }
+
+    res.send(`O resultado da ${tipo} é: ${resultado}`);
+});
+app.listen(porta, () => {
+    console.log(`Exercício 3 na porta ${porta}`);
     });
