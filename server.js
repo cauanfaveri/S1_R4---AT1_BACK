a4074d4b22907dd83a2fec508465e6322de7f7ac
cauanfaveri <cauanfaveri05@gmail.com>
2025-09-18 09:21:41 -0300

    Finalização do exercício 2

--- server.js
+++ server.js
@@ -1,4 +1,28 @@
 const express = require('express');
 const app = express();
 const fs = require('fs/promises');
-const port = 3000;
+const porta = 3000;
+
+app.get('/calculadora', (req, res) => {
+    const {operacao, nUm, nDois} = req.query
+    if (operacao === 'soma'){
+        resultado = Number(nUm) + Number(nDois)
+        return res.json({resultado});
+    }
+    if (operacao === 'subtracao'){
+        resultado = Number(nUm) - Number(nDois)
+        return res.json({resultado});
+    }
+    if (operacao === 'multiplicacao'){
+        resultado = Number(nUm) * Number(nDois)
+        return res.json({resultado});
+    }
+    if (operacao === 'divisao'){
+        resultado = Number(nUm) / Number(nDois)
+        return res.json({resultado});
+    }
+
+});
+app.listen(porta, () => {
+    console.log(`Exercício de soma na porta ${porta}`);
+}); 
