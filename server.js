05fe83dd13f8322e9e45c8f8229b8ff3558ae0b5
cauanfaveri <cauanfaveri05@gmail.com>
2025-09-18 11:09:21 -0300

    FIm ex5

--- server.js
+++ server.js
@@ -1,3 +1,25 @@
 const express = require('express');
 const app = express();
-const port = 5000;
+const porta = 5100;
+
+app.get('/saudacao/:nome', (req, res) => {
+    try {
+        const nome = req.params.nome;
+        const hora = parseInt(req.query.hora);
+        let saudacao = '';
+
+        if (hora >= 4 && hora < 13) {
+            saudacao = 'Bom dia';
+        } 
+        if (hora >= 13 && hora < 18) {
+            saudacao = 'Boa tarde';
+        } 
+        if (hora >= 18 || hora < 3) {
+            saudacao = 'Boa noite';
+        }
+
+        res.status(200).send(`${saudacao}, ${nome}!`);
+    } catch (error) {
+        res.status(500).send('Erro: Falha no servidor.');
+    }
+});
