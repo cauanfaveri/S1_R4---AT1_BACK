d225ed9756f3b7cb6ade7f1f462b073c87e670a2
cauanfaveri <cauanfaveri05@gmail.com>
2025-09-18 08:47:57 -0300

    Finalizando o Exercício 1

--- server.js
+++ server.js
@@ -8,7 +8,27 @@ app.get('/soma/:nUm/:nDois', (req, res) => {
     const resultado = Number(nUm) + Number (nDois);
     res.json({ resultado })
 })
+//Subtração
+app.get('/menos/:nUm/:nDois', (req, res) => {
+    const {nUm, nDois} = req.params;
+    const resultado = Number(nUm) - Number (nDois);
+    res.json({ resultado })
+})
+
+//Multiplicação
+app.get('/multiplicacao/:nUm/:nDois', (req, res) => {
+    const {nUm, nDois} = req.params;
+    const resultado = Number(nUm) * Number (nDois);
+    res.json({ resultado })
+})
+
+//Divisão
+app.get('/divisao/:nUm/:nDois', (req, res) => {
+    const {nUm, nDois} = req.params;
+    const resultado = Number(nUm) / Number (nDois);
+    res.json({ resultado })
+})
+
 app.listen(porta, () => {
     console.log(`Exercício de soma na porta ${porta}`);
 });
-
